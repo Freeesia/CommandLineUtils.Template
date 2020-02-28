69a44d19e7d39bc20f072c78bfd5e73aabfc55de
Freesia <freesia@studiofreesia.com>
2020-02-29 01:30:47 +0900

    初期コミット

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandLineUtilsTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
